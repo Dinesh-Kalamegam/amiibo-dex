534b8ea3d3a55c8b75cb884379dae49bd77797d9
Dinesh-Kalamegam <dinesh.kalamegam@gmail.com>
2021-07-15 14:10:34 +0100

    basic Material UI set up

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+// import Button from '@material-ui/core/Button';
+import { createTheme } from '@material-ui/core/styles';
+import { Paper, Switch, Typography, ThemeProvider, CssBaseline } from '@material-ui/core';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
+  const theme = createTheme({
+    palette: {
+      type: darkMode ? 'dark' : 'light',
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Paper style={{ height: '100vh' }}>
+        <Typography variant="h1">
+          {darkMode ? 'Dark Mode' : 'Light Mode'}
+        <Switch checked={darkMode} onChange={() => setDarkMode(!darkMode)} />
+        </Typography>
+      </Paper>
+    </ThemeProvider>
   );
 }
 

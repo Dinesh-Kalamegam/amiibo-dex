0422db4533e719b18443b3116a14232c97d5e529
Dinesh-Kalamegam <dinesh.kalamegam@gmail.com>
2021-07-16 20:03:30 +0100

    added basic pagination

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import { useState, useEffect } from 'react';
 import { createTheme } from '@material-ui/core/styles';
 import { Paper, Switch, Typography, ThemeProvider, CssBaseline, Grid } from '@material-ui/core';
+import { Pagination } from '@material-ui/lab';
 import axios from 'axios'
 import AmiiboCard from './components/AmiiboCard'
 
 function App() {
   const [darkMode, setDarkMode] = useState(false)
   const [amiiboData, setAmiiboData] = useState(null)
+  const [amiiLength, setAmiiLength] = useState(1)
+  const [page,setPage] = useState(1)
+
+  const handlePageChange = (event, value) => {
+    setPage(value);
+  };
+
   const theme = createTheme({
     palette: {
       type: darkMode ? 'dark' : 'light',
@@ -17,9 +25,11 @@ function App() {
     axios.get("https://amiiboapi.com/api/amiibo/")
       .then(res => {
         setAmiiboData(res.data.amiibo)
+        setAmiiLength(res.data.amiibo.length)
       })
       .catch(error => console.log(error))
   }, [])
+
 
   return (
     <ThemeProvider theme={theme}>
@@ -37,10 +47,11 @@ function App() {
           spacing={1}
           pagination
         >
-          {amiiboData && amiiboData.map(amiibo => (
+          {amiiboData && amiiboData.slice(1+(10*(page-1)),10+(10*(page-1))).map(amiibo => (
             <AmiiboCard amiibo={amiibo} />
           ))}
         </Grid>
+        <Pagination count={Math.floor(amiiLength/10)} onChange={handlePageChange}/>
       </Paper>
     </ThemeProvider >
   );

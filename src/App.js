e1f93feb291bb417b86c33059db1bfb7ba08a207
Dinesh-Kalamegam <dinesh.kalamegam@gmail.com>
2021-07-19 11:23:25 +0100

    CircularProgress import refactor

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import { useState, useEffect } from 'react';
-import { Paper, Switch, Typography, ThemeProvider, CssBaseline } from '@material-ui/core';
+import { Paper, Switch, Typography, ThemeProvider, CssBaseline , CircularProgress } from '@material-ui/core';
 import { createTheme, makeStyles } from '@material-ui/core/styles';
 import { Pagination } from '@material-ui/lab';
 import axios from 'axios'
 import AmiiboCard from './components/AmiiboCard'
-import CircularProgress from '@material-ui/core/CircularProgress';
 
 
 const useStyles = makeStyles(() => ({
